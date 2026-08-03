4ffdd47520e44f070852216459197ca91eb948cd
Jesiel Silva <jesiel.silva@example.com>
2026-08-02 21:53:48 -0400

    fix: SVG do logo hidden no mobile, restaura seta no desktop
    - SVG: hidden lg:block (escondido no mobile, visivel no desktop)
    - Restaura seta e gradiente da logo original no desktop

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -31,7 +31,6 @@ export function Logo({
     lg: "text-[10px] tracking-[0.28em] mt-[2px]",
   };
 
-  // Default to showing subtitle for md/lg if not explicitly passed
   const shouldShowSubtitle = showSubtitle ?? (size !== "sm");
 
   return (
@@ -40,43 +39,61 @@ export function Logo({
         viewBox="0 0 100 100"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className={cn("shrink-0", iconSizes[size])}
+        className={cn("shrink-0 hidden lg:block", iconSizes[size])}
       >
-          <defs>
-            <linearGradient
-              id="logo-left-wing-grad"
-              x1="15"
-              y1="45"
-              x2="35"
-              y2="65"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop offset="0%" stopColor="#0073BC" />
-              <stop offset="100%" stopColor="#054388" />
-            </linearGradient>
+        <defs>
+          <linearGradient
+            id="logo-left-wing-grad"
+            x1="15"
+            y1="45"
+            x2="35"
+            y2="65"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0%" stopColor="#0073BC" />
+            <stop offset="100%" stopColor="#054388" />
+          </linearGradient>
 
-            <linearGradient
-              id="logo-bottom-fold-grad"
-              x1="20"
-              y1="70"
-              x2="45"
-              y2="55"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop offset="0%" stopColor="#031F44" />
-              <stop offset="100%" stopColor="#005A9C" />
-            </linearGradient>
-          </defs>
+          <linearGradient
+            id="logo-bottom-fold-grad"
+            x1="20"
+            y1="70"
+            x2="45"
+            y2="55"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0%" stopColor="#031F44" />
+            <stop offset="100%" stopColor="#005A9C" />
+          </linearGradient>
 
-          <path
-            d="M 12 55 L 26 69 L 38 57 L 24 43 Z"
-            fill="url(#logo-left-wing-grad)"
-          />
-          <path
-            d="M 26 69 L 41 84 L 53 72 L 38 57 Z"
-            fill="url(#logo-bottom-fold-grad)"
-          />
-        </svg>
+          <linearGradient
+            id="logo-arrow-grad"
+            x1="30"
+            y1="80"
+            x2="90"
+            y2="20"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop offset="0%" stopColor="#054388" />
+            <stop offset="35%" stopColor="#0073BC" />
+            <stop offset="70%" stopColor="#00A0E3" />
+            <stop offset="100%" stopColor="#009B9E" />
+          </linearGradient>
+        </defs>
+
+        <path
+          d="M 12 55 L 26 69 L 38 57 L 24 43 Z"
+          fill="url(#logo-left-wing-grad)"
+        />
+        <path
+          d="M 26 69 L 41 84 L 53 72 L 38 57 Z"
+          fill="url(#logo-bottom-fold-grad)"
+        />
+        <path
+          d="M 33 76 L 73 36 L 68 31 L 90 22 L 90 44 L 85 39 L 45 79 Z"
+          fill="url(#logo-arrow-grad)"
+        />
+      </svg>
 
       {showText && (
         <div className="flex flex-col justify-center">
